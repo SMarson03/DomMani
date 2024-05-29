17ab44a24b9636639002c81b0cf91ef2d087c220
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-05-28 20:49:27 -0400

    part 2

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const mainEl = document.querySelector('main')
 console.log(mainEl)
 
-// Set the background color of mainEl to the value stored in the --main-bg CSS custom property
+// Set the background color of mainEl to the value stored in the 
+//--main-bg CSS custom property
 mainEl.style.background = "#4a4e4d";
 
 // Set the content of mainEl to <h1>DOM Manipulation</h1>
@@ -12,3 +13,17 @@ mainEl.appendChild(new_element)
 
 // Add a class of flex-ctr to mainEl
 mainEl.classList = "flex-ctr";
+
+//Select and cache the <nav id="top-menu"> element in a variable 
+//named topMenuEl.
+const topMenuEl = document.querySelector('nav')
+
+//Set the height of the topMenuEl element to be 100%.
+topMenuEl.style.height = "100vh";
+
+//Set the background color of topMenuEl to the value stored in the 
+//--top-menu-bg CSS custom property.
+topMenuEl.style.background = "#0e9aa7";
+
+//Add a class of flex-around to topMenuEl.
+topMenuEl.classList = "flex-around"
